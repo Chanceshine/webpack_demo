3b19b6e0fe1b26ff106c765e5dae1c697802958f
Yu <2399810324@qq.com>
2017-05-19 09:38:42 +0800

    entry
    多页面，entry可以是一个对象，多个输入对应多个输出，hash是每个文件的版本号，hash改为chunkhash则只有当文件改变时，其版本才会发生改变

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 var path = require('path');
 
 module.exports = {
-	entry: ['./src/script/main.js','./src/script/a.js'],
+	entry: {
+		main: './src/script/main.js',
+		a: './src/script/a.js'
+	},
 	output: {
 		path: path.join(__dirname, './dist'),
-		filename: 'bundle.js'
+		filename: '[name]-[hash].js'
 	}
 }
