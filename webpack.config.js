9e6b21e20e23d369615578cde63ba77774b9b898
Yu <2399810324@qq.com>
2017-05-19 13:02:03 +0800

    多页面应用
    多个htmlwebpackplugin对应多个页面，通过chunks参数，只引入指定的js文件

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,36 @@ var htmlWebpackPlugin = require('html-webpack-plugin');	//插件的引用，comm
 module.exports = {
 	entry: {
 		main: './src/script/main.js',
-		a: './src/script/a.js'
+		a: './src/script/a.js',
+		b: './src/script/b.js',
+		c: './src/script/c.js',
 	},
 	output: {
 		path: path.join(__dirname, './dist'),
-		filename: 'js/[name]-[hash].js',		//js文件生成在js目录，其他文件生成在dist目录
+		filename: 'js/[name]-[chunkhash].js',		//js文件生成在js目录，其他文件生成在dist目录
 		publicPath: 'http://cdn.com'		//上线地址
 	},
 	plugins: [
 		new htmlWebpackPlugin({
-			filename: 'index.html',	//设置生成文件名
+			filename: 'a.html',	//设置生成文件名
 			template: 'index.html',		//以index.html为模板文件生成dist目录下的index.html
-			inject: false,		//定义生成js的插入位置，head,body,false
-			title: 'wepack is good',		//index.html模板可以取出这个值
-			date: new Date(),
-			minify: {
-				removeComments: true,
-				collapseWhitespace: true
-			}
+			inject: 'body',		//定义生成js的插入位置，head,body,false
+			title: 'wepack is a',		//index.html模板可以取出这个值
+			chunks: ['main','a']	//只引入指定文件
+		}),
+		new htmlWebpackPlugin({
+			filename: 'b.html',	//设置生成文件名
+			template: 'index.html',		//以index.html为模板文件生成dist目录下的index.html
+			inject: 'body',		//定义生成js的插入位置，head,body,false
+			title: 'wepack is b',		//index.html模板可以取出这个值
+			chunks: ['b']
+		}),
+		new htmlWebpackPlugin({
+			filename: 'c.html',	//设置生成文件名
+			template: 'index.html',		//以index.html为模板文件生成dist目录下的index.html
+			inject: 'body',		//定义生成js的插入位置，head,body,false
+			title: 'wepack is c',		//index.html模板可以取出这个值
+			chunks: ['c']
 		})
 	]
 }
