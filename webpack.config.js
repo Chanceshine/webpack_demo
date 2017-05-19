7b8fcc454bb5cd055ad2e9b02d116a9d32c8d63b
Yu <2399810324@qq.com>
2017-05-19 10:16:46 +0800

    plugin
    以index.hmtl为模板

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');	//插件的引用，commonjs模块引用的写法 
 
 module.exports = {
 	entry: {
@@ -8,5 +9,10 @@ module.exports = {
 	output: {
 		path: path.join(__dirname, './dist'),
 		filename: '[name]-[hash].js'
-	}
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			template: 'index.html'	//以index.html为模板文件生成dist目录下的index.html
+		})
+	]
 }
